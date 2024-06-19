b7c08913f9214a03075507567f3404403809a396
emondaslearner <emon.1000das@gmail.com>
2024-06-19 11:43:24 +0600

    backend: updated get all user apis logic

--- backend/src/lib/v1/user/getAllUser.js
+++ backend/src/lib/v1/user/getAllUser.js
@@ -1,4 +1,5 @@
 const User = require("@models/User");
+const Friend = require("@models/Friend");
 const { error, functions } = require("@utils");
 const { getDataFromRedis } = require("@third-party/redis");
 
@@ -13,24 +14,47 @@ const getAllUser = async ({ filterData, userId }) => {
 
   const filter = { _id: { $ne: userId } };
 
-  const getUsers = async () => {
-    return await User.find(filter)
-      .select("name profile_picture")
-      .sort(sortStr)
-      .skip(filterData.page * filterData.limit - filterData.limit)
-      .limit(filterData.limit);
-  };
+  // Step 1: Retrieve users based on filter, sort, and pagination criteria
+  const users = await User.find(filter)
+    .select("name profile_picture")
+    .sort(sortStr)
+    .skip(filterData.page * filterData.limit - filterData.limit)
+    .limit(filterData.limit)
+    .lean(); // Use .lean() to get plain JavaScript objects instead of Mongoose documents
+
+  // Step 2: Retrieve all friend relationships for the current user
+  const friends = await Friend.find({
+    $or: [{ first_user: userId }, { second_user: userId }],
+  }).lean();
+
+  // Step 3: Add the 'friend' property to each user
+  const getUsers = (async = () => {
+    const usersWithFriendStatus = users.map((user) => {
+      const isFriend = friends.some(
+        (friend) =>
+          (friend.first_user.toString() === user._id.toString() ||
+            friend.second_user.toString() === user._id.toString()) &&
+          !friend.blocked
+      );
+      return {
+        ...user,
+        friend: isFriend,
+      };
+    });
+
+    return usersWithFriendStatus;
+  });
 
   // check in redis
   const serializedFilterData = JSON.stringify(filterData);
   const keyPrefix = "users:";
   const key = `${keyPrefix}${serializedFilterData}${userId}`;
 
-  const users = await getDataFromRedis(key, getUsers);
+  const usersData = await getDataFromRedis(key, getUsers);
 
   const counts = await functions.countEntities(User, filter);
 
-  return { users, counts };
+  return { users: usersData, counts };
 };
 
 module.exports = getAllUser;
