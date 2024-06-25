42452306a4f19e2127c4d19c665e2299df8d8f95
emondaslearner <emon.1000das@gmail.com>
2024-06-25 15:58:04 +0600

    backend: updated api for get single friend request

--- backend/src/lib/v1/friendRequest/getSingleRequest.js
+++ backend/src/lib/v1/friendRequest/getSingleRequest.js
@@ -4,8 +4,8 @@ const { error } = require("@utils");
 const getSingleRequest = async ({ sent_to, sent_by }) => {
   if (!sent_to || !sent_by) {
     throw error.badRequest(
-      `${!sent_to && "sent_to field is missing"}|${
-        !sent_by && "sent_by field is missing"
+      `${!sent_to && "sent_to:sent_to field is missing"}|${
+        !sent_by && "sent_to:sent_by field is missing"
       }`
     );
   }
